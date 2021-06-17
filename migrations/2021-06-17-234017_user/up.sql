f9906a1796ff6ad83e0c096b9658be2905097457
tolumide-ng <tolumideshopein@gmail.com>
2021-06-18 00:48:03 +0100

    country_code and user schema:

---
+++ migrations/2021-06-17-234017_user/up.sql
@@ -0,0 +1,13 @@
+-- Your SQL goes here
+CREATE TABLE user {
+    id SERIAL PRIMARY KEY
+    user_id uuid DEFAULT uuid_generate_v4()
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    email VARCHAR UNIQUE NOT NULL,
+    phone VARCHAR NOT NULL,
+    phone_code INTEGER NOT NULL,
+    -- FOREIGN KEY (phone_code) REFERENCES country_code,
+    -- implement the country, state/location with neo4j
+
+}
