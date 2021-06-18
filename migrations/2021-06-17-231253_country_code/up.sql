1c358edce58fdaf8f6165fd68c1b1d410c1b9090
tolumide-ng <tolumideshopein@gmail.com>
2021-06-18 09:59:19 +0100

    creates user_info, user_type, and country_code schema:

--- migrations/2021-06-17-231253_country_code/up.sql
+++ migrations/2021-06-17-231253_country_code/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
-CREATE TABLE country_code {
+CREATE TABLE country_code (
     id SERIAL PRIMARY KEY,
     country_code VARCHAR NOT NULL,
     iso_code VARCHAR NOT NULL,
     country VARCHAR NOT NULL
-}
+)
--- src/schema.rs
+++ src/schema.rs
@@ -1,7 +1,36 @@
 table! {
+    country_code (id) {
+        id -> Int4,
+        country_code -> Varchar,
+        iso_code -> Varchar,
+        country -> Varchar,
+    }
+}
+
+table! {
+    user_info (id) {
+        id -> Int4,
+        user_id -> Nullable<Uuid>,
+        first_name -> Varchar,
+        last_name -> Varchar,
+        email -> Varchar,
+        phone -> Varchar,
+        phone_code -> Int4,
+    }
+}
+
+table! {
     user_type (id) {
         id -> Int4,
         #[sql_name = "type"]
         type_ -> Varchar,
     }
 }
+
+joinable!(user_info -> country_code (phone_code));
+
+allow_tables_to_appear_in_same_query!(
+    country_code,
+    user_info,
+    user_type,
+);
